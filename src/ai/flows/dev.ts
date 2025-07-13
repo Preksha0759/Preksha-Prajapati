aa0333b7ae0900d3dc30b990c9ff07d00d93658e
Preksha Prajapati <168819297+Preksha0759@users.noreply.github.com>
2025-07-13 23:31:31 +0530

    Delete src/ai directory

--- src/ai/flows/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/suggest-project-ideas.ts';
-import '@/ai/flows/generate-event-image.ts';
-import '@/ai/flows/generate-project-roadmap.ts';
-import '@/ai/flows/ai-helper-flow.ts';
