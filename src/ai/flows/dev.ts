0f2e602dde0275782524ed519ee2533ccb81da86
Preksha Prajapati <168819297+Preksha0759@users.noreply.github.com>
2025-07-13 23:25:15 +0530

    Add files via upload

---
+++ src/ai/flows/dev.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/suggest-project-ideas.ts';
+import '@/ai/flows/generate-event-image.ts';
+import '@/ai/flows/generate-project-roadmap.ts';
+import '@/ai/flows/ai-helper-flow.ts';
